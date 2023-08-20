a9d4c310de6916204fff73da660ddc963c06be46
Kate Skye <heyitskatehere@pm.me>
2023-08-20 02:19:00 -0400

    Renamed Variables in destro.cs Purpose still unclear, needs more prodding imo

--- Scripts/Assembly-UnityScript/Destro.cs
+++ Scripts/Assembly-UnityScript/Destro.cs
@@ -1,92 +1,35 @@
 using System;
 using System.Collections;
-using System.Collections.Generic;
-using System.Runtime.CompilerServices;
-using Boo.Lang;
 using UnityEngine;
 
 [Serializable]
 public class Destro : MonoBehaviour
 {
-	[Serializable]
-	[CompilerGenerated]
-	internal sealed class _0024Start_00241351 : GenericGenerator<WaitForSeconds>
-	{
-		[Serializable]
-		[CompilerGenerated]
-		internal sealed class _0024 : GenericGeneratorEnumerator<WaitForSeconds>, IEnumerator
-		{
-			internal Destro _0024self__00241352;
+    public GameObject[] bars;
+    public float wait = 0.3f;
 
-			public _0024(Destro self_)
-			{
-				_0024self__00241352 = self_;
-			}
+    private IEnumerator Start()
+    {
+        return DestroyObjectAfterDelay();
+    }
 
-			public override bool MoveNext()
-			{
-				int result;
-				switch (_state)
-				{
-				default:
-					if (Network.isServer)
-					{
-						result = (Yield(2, new WaitForSeconds(_0024self__00241352.wait)) ? 1 : 0);
-						break;
-					}
-					goto IL_0052;
-				case 2:
-					Network.Destroy(_0024self__00241352.GetComponent<NetworkView>().viewID);
-					goto IL_0052;
-				case 1:
-					{
-						result = 0;
-						break;
-					}
-					IL_0052:
-					YieldDefault(1);
-					goto case 1;
-				}
-				return (byte)result != 0;
-			}
-		}
+    private IEnumerator DestroyObjectAfterDelay()
+    {
+        if (Network.isServer)
+        {
+            yield return new WaitForSeconds(wait);
+            Network.Destroy(GetComponent<NetworkView>().viewID);
+        }
+    }
 
-		internal Destro _0024self__00241353;
-
-		public _0024Start_00241351(Destro self_)
-		{
-			_0024self__00241353 = self_;
-		}
-
-		public override IEnumerator<WaitForSeconds> GetEnumerator()
-		{
-			return new _0024(_0024self__00241353);
-		}
-	}
-
-	public GameObject[] bars;
-
-	public float wait;
-
-	public Destro()
-	{
-		bars = new GameObject[2];
-		wait = 0.3f;
-	}
-
-	public virtual IEnumerator Start()
-	{
-		return new _0024Start_00241351(this).GetEnumerator();
-	}
-
-	[RPC]
-	public virtual void SetHH(int a)
-	{
-		if (GetComponent<NetworkView>().isMine)
-		{
-			bars[0].SendMessage("SetHH", (float)a * 0.5f);
-			bars[1].SendMessage("SetHH", (float)a * 0.5f);
-		}
-	}
-
-	}
+    [RPC]
+    private void SetHH(int a)
+    {
+        if (GetComponent<NetworkView>().isMine)
+        {
+            float adjustedValue = a * 0.5f;
+            bars[0].SendMessage("SetHH", adjustedValue);
+            bars[1].SendMessage("SetHH", adjustedValue);
+        }
+    }
+}
