bcaa673160075930c9b61e0d24f72bb9aecfbced
Kate Skye <heyitskatehere@pm.me>
2023-12-27 16:22:29 -0500

    is this a typo? why 2 t's?

--- Scripts/Assembly-UnityScript/FlameScriptt.cs
+++ Scripts/Assembly-UnityScript/FlameScriptt.cs
@@ -2,77 +2,32 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
-using Boo.Lang;
 using UnityEngine;
 
 [Serializable]
 public class FlameScriptt : MonoBehaviour
 {
-	[Serializable]
-	[CompilerGenerated]
-	internal sealed class _0024Start_00241510 : GenericGenerator<WaitForSeconds>
-	{
-		[Serializable]
-		[CompilerGenerated]
-		internal sealed class _0024 : GenericGeneratorEnumerator<WaitForSeconds>, IEnumerator
-		{
-			internal FlameScriptt _0024self__00241511;
+    [SerializeField] private GameObject baseObject;
 
-			public _0024(FlameScriptt self_)
-			{
-				_0024self__00241511 = self_;
-			}
+    private IEnumerator StartCoroutine()
+    {
+        yield return new WaitForSeconds(UnityEngine.Random.Range(0, 3) * 0.5f);
+        yield return new WaitForSeconds(UnityEngine.Random.Range(0, 5) * 0.5f);
 
-			public override bool MoveNext()
-			{
-				int result;
-				switch (_state)
-				{
-				default:
-					result = (Yield(2, new WaitForSeconds((float)UnityEngine.Random.Range(0, 3) * 0.5f)) ? 1 : 0);
-					break;
-				case 2:
-					result = (Yield(3, new WaitForSeconds((float)UnityEngine.Random.Range(0, 5) * 0.5f)) ? 1 : 0);
-					break;
-				case 3:
-					if (Network.isServer)
-					{
-						_0024self__00241511.GetComponent<NetworkView>().RPC("pl", RPCMode.All);
-					}
-					YieldDefault(1);
-					goto case 1;
-				case 1:
-					result = 0;
-					break;
-				}
-				return (byte)result != 0;
-			}
-		}
+        if (Network.isServer)
+        {
+            GetComponent<NetworkView>().RPC("PlayAnimation", RPCMode.All);
+        }
+    }
 
-		internal FlameScriptt _0024self__00241512;
+    [RPC]
+    private void PlayAnimation()
+    {
+        baseObject.GetComponent<Animation>().Play(); 
+    }
 
-		public _0024Start_00241510(FlameScriptt self_)
-		{
-			_0024self__00241512 = self_;
-		}
-
-		public override IEnumerator<WaitForSeconds> GetEnumerator()
-		{
-			return new _0024(_0024self__00241512);
-		}
-	}
-
-	public GameObject @base;
-
-	public virtual IEnumerator Start()
-	{
-		return new _0024Start_00241510(this).GetEnumerator();
-	}
-
-	[RPC]
-	public virtual void pl()
-	{
-		@base.GetComponent<Animation>().Play();
-	}
-
-	}
+    private void Start()
+    {
+        StartCoroutine(StartCoroutine());
+    }
+}
