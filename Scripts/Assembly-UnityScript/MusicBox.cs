9ddd0754f36ebc01a8ea4ba8678277115244a667
Kate Skye <heyitskatehere@pm.me>
2023-12-27 19:30:20 -0500

    no boo music

--- Scripts/Assembly-UnityScript/MusicBox.cs
+++ Scripts/Assembly-UnityScript/MusicBox.cs
@@ -8,137 +8,45 @@ using UnityEngine;
 [Serializable]
 public class MusicBox : MonoBehaviour
 {
-	[Serializable]
-	[CompilerGenerated]
-	internal sealed class _0024SetMusic_00242025 : GenericGenerator<WaitForSeconds>
-	{
-		[Serializable]
-		[CompilerGenerated]
-		internal sealed class _0024 : GenericGeneratorEnumerator<WaitForSeconds>, IEnumerator
-		{
-			internal int _0024num_00242026;
+    private const string ResourcesPath = "Resources";
+    
+    private AudioSource audioSource;
+    public AudioClip CurTune { get; private set; }
 
-			internal int _0024a_00242027;
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
 
-			internal MusicBox _0024self__00242028;
+    public void PlayTune(AudioClip clip)
+    {
+        CurTune = clip;
+        audioSource.clip = clip;
+        audioSource.Play();
+    }
 
-			public _0024(int a, MusicBox self_)
-			{
-				_0024a_00242027 = a;
-				_0024self__00242028 = self_;
-			}
+    public AudioClip GetRandomTownTune()
+    {
+        return LoadTune(UnityEngine.Random.Range(10, 13).ToString());
+    }
+    
+    public AudioClip GetBiomeTune(int biome)
+    {
+        return LoadTune(biome.ToString()); 
+    }
 
-			public override bool MoveNext()
-			{
-				int result;
-				switch (_state)
-				{
-				default:
-					if (_0024a_00242027 == 99)
-					{
-						_0024num_00242026 = UnityEngine.Random.Range(10, 13);
-						_0024self__00242028.curTune = (AudioClip)Resources.Load("tune/" + _0024num_00242026);
-					}
-					else
-					{
-						_0024self__00242028.curBiome = _0024a_00242027;
-						_0024self__00242028.curTune = (AudioClip)Resources.Load("tune/" + _0024self__00242028.curBiome);
-					}
-					_0024self__00242028.GetComponent<AudioSource>().clip = _0024self__00242028.curTune;
-					result = (Yield(2, new WaitForSeconds(0.5f)) ? 1 : 0);
-					break;
-				case 2:
-					_0024self__00242028.GetComponent<AudioSource>().Play();
-					YieldDefault(1);
-					goto case 1;
-				case 1:
-					result = 0;
-					break;
-				}
-				return (byte)result != 0;
-			}
-		}
+    private AudioClip LoadTune(string name)
+    {
+        return (AudioClip)Resources.Load(Path.Combine(ResourcesPath, "tune", name));
+    }
+    
+    public void SetTownTune()
+    {
+        PlayTune(GetRandomTownTune());
+    }
 
-		internal int _0024a_00242029;
-
-		internal MusicBox _0024self__00242030;
-
-		public _0024SetMusic_00242025(int a, MusicBox self_)
-		{
-			_0024a_00242029 = a;
-			_0024self__00242030 = self_;
-		}
-
-		public override IEnumerator<WaitForSeconds> GetEnumerator()
-		{
-			return new _0024(_0024a_00242029, _0024self__00242030);
-		}
-	}
-
-	private int curBiome;
-
-	public AudioClip curTune;
-
-	private bool dood;
-
-	private AudioClip prevTune;
-
-	public virtual void Start()
-	{
-		if (Network.isServer)
-		{
-			if (GameScript.isTown)
-			{
-				int num = UnityEngine.Random.Range(10, 13);
-				curTune = (AudioClip)Resources.Load("tune/" + num);
-			}
-			else
-			{
-				curBiome = GameScript.curBiome;
-				curTune = (AudioClip)Resources.Load("tune/" + curBiome);
-			}
-			GetComponent<AudioSource>().clip = curTune;
-		}
-	}
-
-	public virtual void Victory()
-	{
-		GetComponent<AudioSource>().Stop();
-		GetComponent<AudioSource>().clip = (AudioClip)Resources.Load("tune/12");
-		GetComponent<AudioSource>().Play();
-	}
-
-	public virtual IEnumerator SetMusic(int a)
-	{
-		return new _0024SetMusic_00242025(a, this).GetEnumerator();
-	}
-
-	public virtual void Update()
-	{
-		if (PlayerControllerN.isBoss)
-		{
-			SetBoss();
-		}
-	}
-
-	public virtual void SetNormal()
-	{
-		GetComponent<AudioSource>().Stop();
-		GetComponent<AudioSource>().clip = prevTune;
-		GetComponent<AudioSource>().Play();
-	}
-
-	public virtual void SetBoss()
-	{
-		if (!dood)
-		{
-			dood = true;
-			prevTune = curTune;
-			GetComponent<AudioSource>().Stop();
-			curTune = (AudioClip)Resources.Load("tune/boss1");
-			GetComponent<AudioSource>().clip = curTune;
-			GetComponent<AudioSource>().Play();
-		}
-	}
-
-	}
+    public void SetBiomeTune(int biome)
+    {
+        PlayTune(GetBiomeTune(biome));
+    }
+}
