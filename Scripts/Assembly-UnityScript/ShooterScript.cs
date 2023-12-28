b08d8f08517a18826e2b0a02f19ee79f020bd7db
Kate Skye <heyitskatehere@pm.me>
2023-12-27 23:51:50 -0500

    ShooterScript Deboo

--- Scripts/Assembly-UnityScript/ShooterScript.cs
+++ Scripts/Assembly-UnityScript/ShooterScript.cs
@@ -2,73 +2,34 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
-using Boo.Lang;
 using UnityEngine;
 
 [Serializable]
 public class ShooterScript : MonoBehaviour
 {
-	[Serializable]
-	[CompilerGenerated]
-	internal sealed class _0024Attack_00242425 : GenericGenerator<WaitForSeconds>
+	[RPC]
+	public virtual IEnumerator Attack()
 	{
-		[Serializable]
-		[CompilerGenerated]
-		internal sealed class _0024 : GenericGeneratorEnumerator<WaitForSeconds>, IEnumerator
-		{
-			internal ShooterScript _0024self__00242426;
-
-			public _0024(ShooterScript self_)
-			{
-				_0024self__00242426 = self_;
+		while (true) {
+			if (Network.isServer) {
+				yield return new WaitForSeconds(UnityEngine.Random.Range(0, 5) * 0.5f);
+				
+				GetComponent<Animation>().Play("i");
+				
+				yield return new WaitForSeconds(UnityEngine.Random.Range(0, 4) * 0.5f);
+				
+				yield return new WaitForSeconds(1f);
+				
+				GetComponent<Animation>().Play("a");
+				GetComponent<AudioSource>().PlayOneShot(a);
+			
+				yield return new WaitForSeconds(0.5f);
+				
+				Network.Instantiate(blade, transform.position, Quaternion.identity, 0);
 			}
-
-			public override bool MoveNext()
-			{
-				int result;
-				switch (_state)
-				{
-				default:
-					if (Network.isServer)
-					{
-						result = (Yield(2, new WaitForSeconds((float)UnityEngine.Random.Range(0, 5) * 0.5f)) ? 1 : 0);
-						break;
-					}
-					YieldDefault(1);
-					goto case 1;
-				case 2:
-					_0024self__00242426.@base.GetComponent<Animation>().Play("i");
-					result = (Yield(3, new WaitForSeconds((float)UnityEngine.Random.Range(0, 4) * 0.5f)) ? 1 : 0);
-					break;
-				case 3:
-					result = (Yield(4, new WaitForSeconds(1f)) ? 1 : 0);
-					break;
-				case 4:
-					_0024self__00242426.@base.GetComponent<Animation>().Play("a");
-					_0024self__00242426.GetComponent<AudioSource>().PlayOneShot(_0024self__00242426.a);
-					result = (Yield(5, new WaitForSeconds(0.5f)) ? 1 : 0);
-					break;
-				case 5:
-					Network.Instantiate(_0024self__00242426.blade, _0024self__00242426.t.position, Quaternion.identity, 0);
-					goto case 3;
-				case 1:
-					result = 0;
-					break;
-				}
-				return (byte)result != 0;
+			else {
+				yield return null;
 			}
-		}
-
-		internal ShooterScript _0024self__00242427;
-
-		public _0024Attack_00242425(ShooterScript self_)
-		{
-			_0024self__00242427 = self_;
-		}
-
-		public override IEnumerator<WaitForSeconds> GetEnumerator()
-		{
-			return new _0024(_0024self__00242427);
 		}
 	}
 
@@ -96,11 +57,4 @@ public class ShooterScript : MonoBehaviour
 	{
 		StartCoroutine_Auto(Attack());
 	}
-
-	[RPC]
-	public virtual IEnumerator Attack()
-	{
-		return new _0024Attack_00242425(this).GetEnumerator();
-	}
-
-	}
+}
