00d1c628a9d57f868782ec08c9904408d13db61e
Kate Skye <heyitskatehere@pm.me>
2023-12-27 23:26:04 -0500

    debood the stonescript

--- Scripts/Assembly-UnityScript/StoneScript.cs
+++ Scripts/Assembly-UnityScript/StoneScript.cs
@@ -8,64 +8,22 @@ using UnityEngine;
 [Serializable]
 public class StoneScript : MonoBehaviour
 {
-	[Serializable]
-	[CompilerGenerated]
-	internal sealed class _0024Exile_00242623 : GenericGenerator<WaitForSeconds>
+	[RPC]
+	public IEnumerator Exile() 
 	{
-		[Serializable]
-		[CompilerGenerated]
-		internal sealed class _0024 : GenericGeneratorEnumerator<WaitForSeconds>, IEnumerator
-		{
-			internal StoneScript _0024self__00242624;
-
-			public _0024(StoneScript self_)
-			{
-				_0024self__00242624 = self_;
-			}
-
-			public override bool MoveNext()
-			{
-				int result;
-				switch (_state)
-				{
-				default:
-					if (!_0024self__00242624.exiling)
-					{
-						_0024self__00242624.exiling = true;
-						_0024self__00242624.transform.position = new Vector3(0f, 0f, -500f);
-						result = (Yield(2, new WaitForSeconds(4f)) ? 1 : 0);
-						break;
-					}
-					goto IL_008f;
-				case 2:
-					Network.Destroy(_0024self__00242624.GetComponent<NetworkView>().viewID);
-					Network.RemoveRPCs(_0024self__00242624.GetComponent<NetworkView>().viewID);
-					goto IL_008f;
-				case 1:
-					{
-						result = 0;
-						break;
-					}
-					IL_008f:
-					YieldDefault(1);
-					goto case 1;
-				}
-				return (byte)result != 0;
-			}
-		}
-
-		internal StoneScript _0024self__00242625;
-
-		public _0024Exile_00242623(StoneScript self_)
-		{
-			_0024self__00242625 = self_;
-		}
-
-		public override IEnumerator<WaitForSeconds> GetEnumerator()
-		{
-			return new _0024(_0024self__00242625);
+		if (!exiling) {
+			exiling = true;
+			
+			transform.position = new Vector3(0f, 0f, -500f);
+			
+			yield return new WaitForSeconds(4f);
+			
+			NetworkView view = GetComponent<NetworkView>();
+			Network.Destroy(view.viewID);
+			Network.RemoveRPCs(view.viewID);
 		}
 	}
+
 
 	public bool isPoison;
 
@@ -95,10 +53,5 @@ public class StoneScript : MonoBehaviour
 		}
 	}
 
-	[RPC]
-	public virtual IEnumerator Exile()
-	{
-		return new _0024Exile_00242623(this).GetEnumerator();
-	}
 
 	}
