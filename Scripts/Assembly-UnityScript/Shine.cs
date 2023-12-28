3fb4fdf2f6ca7de00e80cc9f3829dbcfdedf1d48
Kate Skye <heyitskatehere@pm.me>
2023-12-27 23:43:34 -0500

    shine deboo

--- Scripts/Assembly-UnityScript/Shine.cs
+++ Scripts/Assembly-UnityScript/Shine.cs
@@ -2,110 +2,52 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
-using Boo.Lang;
 using UnityEngine;
 
 [Serializable]
 public class Shine : MonoBehaviour
 {
-	[Serializable]
-	[CompilerGenerated]
-	internal sealed class _0024Animate_00242419 : GenericGenerator<WaitForSeconds>
-	{
-		[Serializable]
-		[CompilerGenerated]
-		internal sealed class _0024 : GenericGeneratorEnumerator<WaitForSeconds>, IEnumerator
-		{
-			internal int _0024i_00242420;
+    private IEnumerator AnimateTexture() 
+    {
+        var r = GetComponent<Renderer>();
+        
+        for(int i = 0; i < 4; i++) 
+        {
+            r.material.mainTextureOffset = new Vector2(
+                r.material.mainTextureOffset.x + 0.25f, 
+                r.material.mainTextureOffset.y);
+                
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
 
-			internal float _0024_0024871_00242421;
+    public AudioClip soun;
 
-			internal Vector2 _0024_0024872_00242422;
+    public int ra;
+    
+    private Renderer r;
 
-			internal Shine _0024self__00242423;
+    public GameObject g;
 
-			public _0024(Shine self_)
-			{
-				_0024self__00242423 = self_;
-			}
+    private MenuScript gameScript;
 
-			public override bool MoveNext()
-			{
-				int result;
-				switch (_state)
-				{
-				default:
-					_0024self__00242423.r = _0024self__00242423.GetComponent<Renderer>();
-					_0024i_00242420 = default(int);
-					goto IL_0042;
-				case 2:
-					_0024i_00242420 = 0;
-					goto IL_0113;
-				case 3:
-					_0024i_00242420++;
-					goto IL_0113;
-				case 1:
-					{
-						result = 0;
-						break;
-					}
-					IL_0113:
-					if (_0024i_00242420 < 4)
-					{
-						float num = (_0024_0024871_00242421 = _0024self__00242423.r.material.mainTextureOffset.x + 0.25f);
-						Vector2 vector = (_0024_0024872_00242422 = _0024self__00242423.r.material.mainTextureOffset);
-						float num2 = (_0024_0024872_00242422.x = _0024_0024871_00242421);
-						Vector2 vector3 = (_0024self__00242423.r.material.mainTextureOffset = _0024_0024872_00242422);
-						result = (Yield(3, new WaitForSeconds(0.1f)) ? 1 : 0);
-						break;
-					}
-					goto IL_0042;
-					IL_0042:
-					result = (Yield(2, new WaitForSeconds((float)UnityEngine.Random.Range(2, 6) * 0.4f)) ? 1 : 0);
-					break;
-				}
-				return (byte)result != 0;
-			}
-		}
+    public int type;
 
-		internal Shine _0024self__00242424;
+    public virtual void Awake()
+    {
+        gameScript = (MenuScript)g.GetComponent("MenuScript");
+    }
 
-		public _0024Animate_00242419(Shine self_)
-		{
-			_0024self__00242424 = self_;
-		}
+    public virtual void OnEnable()
+    {        
+    }
 
-		public override IEnumerator<WaitForSeconds> GetEnumerator()
-		{
-			return new _0024(_0024self__00242424);
-		}
-	}
+    public virtual IEnumerator Animate()
+    {
+        return AnimateTexture();
+    }
+    
 
-	public AudioClip soun;
-
-	public int ra;
-
-	private Renderer r;
-
-	public GameObject g;
-
-	private MenuScript gameScript;
-
-	public int type;
-
-	public virtual void Awake()
-	{
-		gameScript = (MenuScript)g.GetComponent("MenuScript");
-	}
-
-	public virtual void OnEnable()
-	{
-	}
-
-	public virtual IEnumerator Animate()
-	{
-		return new _0024Animate_00242419(this).GetEnumerator();
-	}
 
 	public virtual void OnMouseEnter()
 	{
