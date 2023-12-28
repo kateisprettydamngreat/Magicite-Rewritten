98651d906d4982ecc1a15c97ae0db8e1ca7e1bd1
Kate Skye <heyitskatehere@pm.me>
2023-12-27 20:22:35 -0500

    ore-ptimized

--- Scripts/Assembly-UnityScript/OreScript.cs
+++ Scripts/Assembly-UnityScript/OreScript.cs
@@ -8,67 +8,22 @@ using UnityEngine;
 [Serializable]
 public class OreScript : MonoBehaviour
 {
-	[Serializable]
-	[CompilerGenerated]
-	internal sealed class _0024Exile_00242117 : GenericGenerator<WaitForSeconds>
-	{
-		[Serializable]
-		[CompilerGenerated]
-		internal sealed class _0024 : GenericGeneratorEnumerator<WaitForSeconds>, IEnumerator
-		{
-			internal OreScript _0024self__00242118;
+    [RPC]
+    public virtual IEnumerator Exile()
+    {
+        if (!exiling)
+        {
+            exiling = true;
+            transform.position = new Vector3(0f, 0f, -500f);
+            yield return new WaitForSeconds(4f);
 
-			public _0024(OreScript self_)
-			{
-				_0024self__00242118 = self_;
-			}
-
-			public override bool MoveNext()
-			{
-				int result;
-				switch (_state)
-				{
-				default:
-					if (!_0024self__00242118.exiling)
-					{
-						_0024self__00242118.exiling = true;
-						_0024self__00242118.transform.position = new Vector3(0f, 0f, -500f);
-						result = (Yield(2, new WaitForSeconds(4f)) ? 1 : 0);
-						break;
-					}
-					goto IL_0099;
-				case 2:
-					if (Network.isServer)
-					{
-						Network.Destroy(_0024self__00242118.GetComponent<NetworkView>().viewID);
-						Network.RemoveRPCs(_0024self__00242118.GetComponent<NetworkView>().viewID);
-					}
-					goto IL_0099;
-				case 1:
-					{
-						result = 0;
-						break;
-					}
-					IL_0099:
-					YieldDefault(1);
-					goto case 1;
-				}
-				return (byte)result != 0;
-			}
-		}
-
-		internal OreScript _0024self__00242119;
-
-		public _0024Exile_00242117(OreScript self_)
-		{
-			_0024self__00242119 = self_;
-		}
-
-		public override IEnumerator<WaitForSeconds> GetEnumerator()
-		{
-			return new _0024(_0024self__00242119);
-		}
-	}
+            if (Network.isServer)
+            {
+                Network.Destroy(GetComponent<NetworkView>().viewID);
+                Network.RemoveRPCs(GetComponent<NetworkView>().viewID);
+            }
+        }
+    }
 
 	public int tier;
 
@@ -118,21 +73,21 @@ public class OreScript : MonoBehaviour
 		GetComponent<NetworkView>().RPC("TD2", RPCMode.All, dmg);
 	}
 
-	public virtual int GetOre(int a)
-	{
-		int num = default(int);
-		return a switch
-		{
-			0 => 4, 
-			1 => 5, 
-			2 => 6, 
-			3 => 60, 
-			4 => 62, 
-			5 => 64, 
-			6 => 66, 
-			_ => 6, 
-		};
-	}
+    public virtual int GetOre(int a)
+    {
+        int num = default(int);
+        return a switch
+        {
+            0 => 4,
+            1 => 5,
+            2 => 6,
+            3 => 60,
+            4 => 62,
+            5 => 64,
+            6 => 66,
+            _ => 6,
+        };
+    }
 
 	[RPC]
 	public virtual void TD2(int dmg)
@@ -197,12 +152,6 @@ public class OreScript : MonoBehaviour
 				StartCoroutine_Auto(Exile());
 			}
 		}
-	}
-
-	[RPC]
-	public virtual IEnumerator Exile()
-	{
-		return new _0024Exile_00242117(this).GetEnumerator();
 	}
 
 	}
