d23e611c4226ff22d47179be7c74f82818da91c1
Kate Skye <heyitskatehere@pm.me>
2024-01-24 16:00:25 -0500

    Deboo TitanTrig.cs

--- Scripts/Assembly-UnityScript/TitanTrig.cs
+++ Scripts/Assembly-UnityScript/TitanTrig.cs
@@ -1,92 +1,37 @@
-using System;
 using System.Collections;
-using System.Collections.Generic;
-using System.Runtime.CompilerServices;
-using Boo.Lang;
 using UnityEngine;
-
-/*////////
-Likely Orphan!
-///////////*/
-
+// likely orphan
 [Serializable]
 public class TitanTrig : MonoBehaviour
 {
-	[Serializable]
-	[CompilerGenerated]
-	internal sealed class _0024Trig_00242664 : GenericGenerator<WaitForSeconds>
-	{
-		[Serializable]
-		[CompilerGenerated]
-		internal sealed class _0024 : GenericGeneratorEnumerator<WaitForSeconds>, IEnumerator
-		{
-			internal TitanTrig _0024self__00242665;
+    public GameObject parent;
 
-			public _0024(TitanTrig self_)
-			{
-				_0024self__00242665 = self_;
-			}
+    private TitanScript titanScript;
+    private GameObject target;
 
-			public override bool MoveNext()
-			{
-				int result;
-				switch (_state)
-				{
-				default:
-					result = (Yield(2, new WaitForSeconds(1f)) ? 1 : 0);
-					break;
-				case 2:
-					_0024self__00242665.GetComponent<Collider>().enabled = false;
-					result = (Yield(3, new WaitForSeconds(0.2f)) ? 1 : 0);
-					break;
-				case 3:
-					_0024self__00242665.GetComponent<Collider>().enabled = true;
-					goto default;
-				case 1:
-					result = 0;
-					break;
-				}
-				return (byte)result != 0;
-			}
-		}
+    public virtual void Awake()
+    {
+        titanScript = parent.GetComponent<TitanScript>();
+        StartCoroutine(Trig());
+    }
 
-		internal TitanTrig _0024self__00242666;
+    private IEnumerator Trig()
+    {
+        while (true)
+        {
+            GetComponent<Collider>().enabled = false;
+            yield return new WaitForSeconds(0.2f);
+            GetComponent<Collider>().enabled = true;
+            yield return new WaitForSeconds(1f);
+        }
+    }
 
-		public _0024Trig_00242664(TitanTrig self_)
-		{
-			_0024self__00242666 = self_;
-		}
-
-		public override IEnumerator<WaitForSeconds> GetEnumerator()
-		{
-			return new _0024(_0024self__00242666);
-		}
-	}
-
-	public GameObject parent;
-
-	private TitanScript titanScript;
-
-	private GameObject target;
-
-	public virtual void Awake()
-	{
-		titanScript = (TitanScript)parent.GetComponent("TitanScript");
-		StartCoroutine_Auto(Trig());
-	}
-
-	public virtual IEnumerator Trig()
-	{
-		return new _0024Trig_00242664(this).GetEnumerator();
-	}
-
-	public virtual void OnTriggerEnter(Collider c)
-	{
-		if (c.gameObject.layer == 8)
-		{
-			target = c.gameObject;
-			titanScript.Attack(target);
-		}
-	}
-
-	}
+    public virtual void OnTriggerEnter(Collider c)
+    {
+        if (c.gameObject.layer == 8)
+        {
+            target = c.gameObject;
+            titanScript.Attack(target);
+        }
+    }
+}
