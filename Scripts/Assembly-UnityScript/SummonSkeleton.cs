fbd12e820816cd4bcf6607f413dcff5e933439bd
Kate Skye <heyitskatehere@pm.me>
2024-01-24 16:48:58 -0500

    Deboo SummonSkeleton.cs

--- Scripts/Assembly-UnityScript/SummonSkeleton.cs
+++ Scripts/Assembly-UnityScript/SummonSkeleton.cs
@@ -2,234 +2,77 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
-using Boo.Lang;
 using UnityEngine;
 
 [Serializable]
 public class SummonSkeleton : MonoBehaviour
 {
-	[Serializable]
-	[CompilerGenerated]
-	internal sealed class _0024Exile_00242626 : GenericGenerator<WaitForSeconds>
-	{
-		[Serializable]
-		[CompilerGenerated]
-		internal sealed class _0024 : GenericGeneratorEnumerator<WaitForSeconds>, IEnumerator
+    public IEnumerator Exile()
+    {
+        exiling = true;
+        transform.position = new Vector3(0f, 0f, -500f);
+        yield return new WaitForSeconds(4f);
+
+        var networkView = GetComponent<NetworkView>();
+        if (networkView)
+        {
+            Network.Destroy(networkView.viewID);
+            Network.RemoveRPCs(networkView.viewID);
+        }
+
+        exiling = false;
+    }
+    public IEnumerator Die()
+    {
+        yield return new WaitForSeconds(this.waitt);
+        if (GetComponent<NetworkView>().isMine)
+        {
+            StartCoroutine(Exile());
+        }
+    }
+	public IEnumerator HitN()
 		{
-			internal SummonSkeleton _0024self__00242627;
-
-			public _0024(SummonSkeleton self_)
+			if (GetComponent<NetworkView>().isMine)
 			{
-				_0024self__00242627 = self_;
+				// Perform raycast checks and send messages
+				CheckAndSendMessage(new Vector3(1f, 0f, 0f));
+				CheckAndSendMessage(new Vector3(-1f, 0f, 0f));
+				
+				// Rotate based on raycast hits
+				RotateOnRaycastHit(new Vector3(1f, 0f, 0f));
+				RotateOnRaycastHit(new Vector3(-1f, 0f, 0f));
+				
+				// Wait for 0.4 seconds
+				yield return new WaitForSeconds(0.4f);
 			}
+		}
 
-			public override bool MoveNext()
+		private void CheckAndSendMessage(Vector3 direction)
+		{
+			Ray ray = new Ray(transform.position, direction);
+			RaycastHit hit;
+			if (Physics.Raycast(ray, out hit, 3f, 512) && (hit.transform.gameObject.layer == 9 || hit.transform.gameObject.layer == 12))
 			{
-				int result;
-				switch (_state)
+				hit.transform.gameObject.SendMessage("TD", DOOD);
+			}
+		}
+
+		private void RotateOnRaycastHit(Vector3 direction)
+		{
+			Ray ray = new Ray(transform.position, direction);
+			RaycastHit hit;
+			if (Physics.Raycast(ray, out hit, 3f, 2048))
+			{
+				if (transform.rotation.y <= 0f)
 				{
-				default:
-					if (!_0024self__00242627.exiling)
-					{
-						_0024self__00242627.exiling = true;
-						_0024self__00242627.t.position = new Vector3(0f, 0f, -500f);
-						result = (Yield(2, new WaitForSeconds(4f)) ? 1 : 0);
-						break;
-					}
-					goto IL_008f;
-				case 2:
-					Network.Destroy(_0024self__00242627.GetComponent<NetworkView>().viewID);
-					Network.RemoveRPCs(_0024self__00242627.GetComponent<NetworkView>().viewID);
-					goto IL_008f;
-				case 1:
-					{
-						result = 0;
-						break;
-					}
-					IL_008f:
-					YieldDefault(1);
-					goto case 1;
+					transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, 180, transform.rotation.eulerAngles.z);
 				}
-				return (byte)result != 0;
-			}
-		}
-
-		internal SummonSkeleton _0024self__00242628;
-
-		public _0024Exile_00242626(SummonSkeleton self_)
-		{
-			_0024self__00242628 = self_;
-		}
-
-		public override IEnumerator<WaitForSeconds> GetEnumerator()
-		{
-			return new _0024(_0024self__00242628);
-		}
-	}
-
-	[Serializable]
-	[CompilerGenerated]
-	internal sealed class _0024Die_00242629 : GenericGenerator<WaitForSeconds>
-	{
-		[Serializable]
-		[CompilerGenerated]
-		internal sealed class _0024 : GenericGeneratorEnumerator<WaitForSeconds>, IEnumerator
-		{
-			internal SummonSkeleton _0024self__00242630;
-
-			public _0024(SummonSkeleton self_)
-			{
-				_0024self__00242630 = self_;
-			}
-
-			public override bool MoveNext()
-			{
-				int result;
-				switch (_state)
+				else
 				{
-				default:
-					result = (Yield(2, new WaitForSeconds(_0024self__00242630.waitt)) ? 1 : 0);
-					break;
-				case 2:
-					if (_0024self__00242630.GetComponent<NetworkView>().isMine)
-					{
-						_0024self__00242630.StartCoroutine_Auto(_0024self__00242630.Exile());
-					}
-					YieldDefault(1);
-					goto case 1;
-				case 1:
-					result = 0;
-					break;
+					transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, 0, transform.rotation.eulerAngles.z);
 				}
-				return (byte)result != 0;
 			}
 		}
-
-		internal SummonSkeleton _0024self__00242631;
-
-		public _0024Die_00242629(SummonSkeleton self_)
-		{
-			_0024self__00242631 = self_;
-		}
-
-		public override IEnumerator<WaitForSeconds> GetEnumerator()
-		{
-			return new _0024(_0024self__00242631);
-		}
-	}
-
-	[Serializable]
-	[CompilerGenerated]
-	internal sealed class _0024HitN_00242632 : GenericGenerator<WaitForSeconds>
-	{
-		[Serializable]
-		[CompilerGenerated]
-		internal sealed class _0024 : GenericGeneratorEnumerator<WaitForSeconds>, IEnumerator
-		{
-			internal int _0024_0024961_00242633;
-
-			internal Quaternion _0024_0024962_00242634;
-
-			internal int _0024_0024963_00242635;
-
-			internal Quaternion _0024_0024964_00242636;
-
-			internal int _0024_0024965_00242637;
-
-			internal Quaternion _0024_0024966_00242638;
-
-			internal int _0024_0024967_00242639;
-
-			internal Quaternion _0024_0024968_00242640;
-
-			internal SummonSkeleton _0024self__00242641;
-
-			public _0024(SummonSkeleton self_)
-			{
-				_0024self__00242641 = self_;
-			}
-
-			public override bool MoveNext()
-			{
-				int result;
-				switch (_state)
-				{
-				default:
-					if (_0024self__00242641.GetComponent<NetworkView>().isMine)
-					{
-						goto case 2;
-					}
-					YieldDefault(1);
-					goto case 1;
-				case 2:
-					_0024self__00242641.ray = new Ray(_0024self__00242641.t.position, new Vector3(1f, 0f, 0f));
-					if (Physics.Raycast(_0024self__00242641.ray, out _0024self__00242641.hit, 3f, 512) && (_0024self__00242641.hit.transform.gameObject.layer == 9 || _0024self__00242641.hit.transform.gameObject.layer == 12))
-					{
-						_0024self__00242641.hit.transform.gameObject.SendMessage("TD", _0024self__00242641.DOOD);
-					}
-					_0024self__00242641.ray = new Ray(_0024self__00242641.t.position, new Vector3(-1f, 0f, 0f));
-					if (Physics.Raycast(_0024self__00242641.ray, out _0024self__00242641.hit, 3f, 512) && (_0024self__00242641.hit.transform.gameObject.layer == 9 || _0024self__00242641.hit.transform.gameObject.layer == 12))
-					{
-						_0024self__00242641.hit.transform.gameObject.SendMessage("TD", _0024self__00242641.DOOD);
-					}
-					_0024self__00242641.ray = new Ray(_0024self__00242641.t.position, new Vector3(1f, 0f, 0f));
-					if (Physics.Raycast(_0024self__00242641.ray, out _0024self__00242641.hit, 3f, 2048))
-					{
-						if (_0024self__00242641.t.rotation.y <= 0f)
-						{
-							int num = (_0024_0024963_00242635 = 180);
-							Quaternion quaternion = (_0024_0024964_00242636 = _0024self__00242641.t.rotation);
-							float num2 = (_0024_0024964_00242636.y = _0024_0024963_00242635);
-							Quaternion quaternion3 = (_0024self__00242641.t.rotation = _0024_0024964_00242636);
-						}
-						else
-						{
-							int num3 = (_0024_0024961_00242633 = 0);
-							Quaternion quaternion4 = (_0024_0024962_00242634 = _0024self__00242641.t.rotation);
-							float num4 = (_0024_0024962_00242634.y = _0024_0024961_00242633);
-							Quaternion quaternion6 = (_0024self__00242641.t.rotation = _0024_0024962_00242634);
-						}
-					}
-					_0024self__00242641.ray = new Ray(_0024self__00242641.t.position, new Vector3(-1f, 0f, 0f));
-					if (Physics.Raycast(_0024self__00242641.ray, out _0024self__00242641.hit, 3f, 2048))
-					{
-						if (_0024self__00242641.t.rotation.y <= 0f)
-						{
-							int num5 = (_0024_0024967_00242639 = 180);
-							Quaternion quaternion7 = (_0024_0024968_00242640 = _0024self__00242641.t.rotation);
-							float num6 = (_0024_0024968_00242640.y = _0024_0024967_00242639);
-							Quaternion quaternion9 = (_0024self__00242641.t.rotation = _0024_0024968_00242640);
-						}
-						else
-						{
-							int num7 = (_0024_0024965_00242637 = 0);
-							Quaternion quaternion10 = (_0024_0024966_00242638 = _0024self__00242641.t.rotation);
-							float num8 = (_0024_0024966_00242638.y = _0024_0024965_00242637);
-							Quaternion quaternion12 = (_0024self__00242641.t.rotation = _0024_0024966_00242638);
-						}
-					}
-					result = (Yield(2, new WaitForSeconds(0.4f)) ? 1 : 0);
-					break;
-				case 1:
-					result = 0;
-					break;
-				}
-				return (byte)result != 0;
-			}
-		}
-
-		internal SummonSkeleton _0024self__00242642;
-
-		public _0024HitN_00242632(SummonSkeleton self_)
-		{
-			_0024self__00242642 = self_;
-		}
-
-		public override IEnumerator<WaitForSeconds> GetEnumerator()
-		{
-			return new _0024(_0024self__00242642);
-		}
-	}
 
 	public bool isZombie;
 
@@ -300,17 +143,6 @@ public class SummonSkeleton : MonoBehaviour
 		StartCoroutine_Auto(Die());
 	}
 
-	[RPC]
-	public virtual IEnumerator Exile()
-	{
-		return new _0024Exile_00242626(this).GetEnumerator();
-	}
-
-	public virtual IEnumerator Die()
-	{
-		return new _0024Die_00242629(this).GetEnumerator();
-	}
-
 	public virtual void Update()
 	{
 		newPos = new Vector3(t.position.x, t.position.y - 1f, 0f);
@@ -336,11 +168,6 @@ public class SummonSkeleton : MonoBehaviour
 			float num4 = (velocity2.x = num3);
 			Vector3 vector4 = (GetComponent<Rigidbody>().velocity = velocity2);
 		}
-	}
-
-	public virtual IEnumerator HitN()
-	{
-		return new _0024HitN_00242632(this).GetEnumerator();
 	}
 
 	public virtual void Hit()
