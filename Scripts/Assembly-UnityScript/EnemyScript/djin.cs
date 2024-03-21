7394705e64724c62a5114546a3826b7db275b2a2
Kate Skye <heyitskatehere@pm.me>
2024-03-20 23:55:52 -0400

    deboo djin

--- Scripts/Assembly-UnityScript/EnemyScript/djin.cs
+++ Scripts/Assembly-UnityScript/EnemyScript/djin.cs
@@ -2,157 +2,84 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
-using Boo.Lang;
 using UnityEngine;
 
 [Serializable]
 public class djin : EnemyScript
 {
-	[Serializable]
-	[CompilerGenerated]
-	internal sealed class _0024Attack_00242847 : GenericGenerator<WaitForSeconds>
-	{
-		[Serializable]
-		[CompilerGenerated]
-		internal sealed class _0024 : GenericGeneratorEnumerator<WaitForSeconds>, IEnumerator
-		{
-			internal GameObject _0024g_00242848;
-
-			internal Vector3 _0024pp2_00242849;
-
-			internal int _0024num_00242850;
-
-			internal Vector3 _0024pp_00242851;
-
-			internal djin _0024self__00242852;
-
-			public _0024(Vector3 pp, djin self_)
-			{
-				_0024pp_00242851 = pp;
-				_0024self__00242852 = self_;
-			}
-
-			public override bool MoveNext()
-			{
-				int result;
-				switch (_state)
-				{
-				default:
-					_0024self__00242852.ATKING = true;
-					if (!(_0024pp_00242851.x <= _0024self__00242852.transform.position.x))
-					{
-						_0024self__00242852.GetComponent<NetworkView>().RPC("Turn", RPCMode.All, 1);
-					}
-					else
-					{
-						_0024self__00242852.GetComponent<NetworkView>().RPC("Turn", RPCMode.All, 0);
-					}
-					_0024g_00242848 = null;
-					result = (Yield(2, new WaitForSeconds(0.3f)) ? 1 : 0);
-					break;
-				case 2:
-					_0024self__00242852.GetComponent<NetworkView>().RPC("A1", RPCMode.All);
-					result = (Yield(3, new WaitForSeconds(0.5f)) ? 1 : 0);
-					break;
-				case 3:
-					if ((bool)_0024self__00242852.player)
-					{
-						_0024pp2_00242849 = _0024self__00242852.player.transform.position;
-						if (Network.isServer)
-						{
-							_0024g_00242848 = (GameObject)Network.Instantiate(Resources.Load("haz/whelpFire"), _0024self__00242852.djinPoint.transform.position, Quaternion.identity, 0);
-							_0024g_00242848.GetComponent<NetworkView>().RPC("Set", RPCMode.All, _0024pp2_00242849);
-							result = (Yield(4, new WaitForSeconds(0.3f)) ? 1 : 0);
-							break;
-						}
-					}
-					goto IL_02ba;
-				case 4:
-					_0024pp2_00242849 = _0024self__00242852.player.transform.position;
-					_0024g_00242848 = (GameObject)Network.Instantiate(Resources.Load("haz/whelpFire"), _0024self__00242852.djinPoint.transform.position, Quaternion.identity, 0);
-					_0024g_00242848.GetComponent<NetworkView>().RPC("Set", RPCMode.All, _0024pp2_00242849);
-					result = (Yield(5, new WaitForSeconds(0.3f)) ? 1 : 0);
-					break;
-				case 5:
-					_0024pp2_00242849 = _0024self__00242852.player.transform.position;
-					_0024g_00242848 = (GameObject)Network.Instantiate(Resources.Load("haz/whelpFire"), _0024self__00242852.djinPoint.transform.position, Quaternion.identity, 0);
-					_0024g_00242848.GetComponent<NetworkView>().RPC("Set", RPCMode.All, _0024pp2_00242849);
-					goto IL_02ba;
-				case 6:
-					_0024self__00242852.atking = false;
-					if ((bool)_0024self__00242852.player)
-					{
-						_0024num_00242850 = UnityEngine.Random.Range(0, 3);
-						if (_0024num_00242850 == 0)
-						{
-							_0024self__00242852.curVector = _0024self__00242852.t.position - _0024self__00242852.player.transform.position;
-						}
-						else
-						{
-							_0024self__00242852.curVector = _0024self__00242852.player.transform.position - _0024self__00242852.t.position;
-						}
-						if (!(_0024self__00242852.player.transform.position.x <= _0024self__00242852.t.position.x))
-						{
-							_0024self__00242852.e.transform.rotation = Quaternion.Euler(0f, 180f, 0f);
-						}
-						else
-						{
-							_0024self__00242852.e.transform.rotation = Quaternion.Euler(0f, 0f, 0f);
-						}
-						_0024self__00242852.atking = true;
-						result = (Yield(7, new WaitForSeconds(2f)) ? 1 : 0);
-						break;
-					}
-					goto case 7;
-				case 7:
-					_0024self__00242852.atking = false;
-					result = (Yield(8, new WaitForSeconds(1.5f)) ? 1 : 0);
-					break;
-				case 8:
-					_0024self__00242852.ATKING = false;
-					YieldDefault(1);
-					goto case 1;
-				case 1:
-					{
-						result = 0;
-						break;
-					}
-					IL_02ba:
-					if ((bool)_0024self__00242852.player)
-					{
-						_0024self__00242852.curVector = _0024self__00242852.t.position - _0024self__00242852.player.transform.position;
-						if (!(_0024self__00242852.player.transform.position.x <= _0024self__00242852.t.position.x))
-						{
-							_0024self__00242852.e.transform.rotation = Quaternion.Euler(0f, 180f, 0f);
-						}
-						else
-						{
-							_0024self__00242852.e.transform.rotation = Quaternion.Euler(0f, 0f, 0f);
-						}
-					}
-					_0024self__00242852.atking = true;
-					result = (Yield(6, new WaitForSeconds(0.5f)) ? 1 : 0);
-					break;
-				}
-				return (byte)result != 0;
-			}
-		}
-
-		internal Vector3 _0024pp_00242853;
-
-		internal djin _0024self__00242854;
-
-		public _0024Attack_00242847(Vector3 pp, djin self_)
-		{
-			_0024pp_00242853 = pp;
-			_0024self__00242854 = self_;
-		}
-
-		public override IEnumerator<WaitForSeconds> GetEnumerator()
-		{
-			return new _0024(_0024pp_00242853, _0024self__00242854);
-		}
-	}
+    public virtual IEnumerator Attack(Vector3 pp)
+    {
+        ATKING = true;
+        if (!(pp.x <= transform.position.x))
+        {
+            GetComponent<NetworkView>().RPC("Turn", RPCMode.All, 1);
+        }
+        else
+        {
+            GetComponent<NetworkView>().RPC("Turn", RPCMode.All, 0);
+        }
+        GameObject g = null;
+        yield return new WaitForSeconds(0.3f);
+        GetComponent<NetworkView>().RPC("A1", RPCMode.All);
+        yield return new WaitForSeconds(0.5f);
+        if ((bool)player)
+        {
+            Vector3 pp2 = player.transform.position;
+            if (Network.isServer)
+            {
+                g = (GameObject)Network.Instantiate(Resources.Load("haz/whelpFire"), djinPoint.transform.position, Quaternion.identity, 0);
+                g.GetComponent<NetworkView>().RPC("Set", RPCMode.All, pp2);
+                yield return new WaitForSeconds(0.3f);
+            }
+            pp2 = player.transform.position;
+            g = (GameObject)Network.Instantiate(Resources.Load("haz/whelpFire"), djinPoint.transform.position, Quaternion.identity, 0);
+            g.GetComponent<NetworkView>().RPC("Set", RPCMode.All, pp2);
+            yield return new WaitForSeconds(0.3f);
+            pp2 = player.transform.position;
+            g = (GameObject)Network.Instantiate(Resources.Load("haz/whelpFire"), djinPoint.transform.position, Quaternion.identity, 0);
+            g.GetComponent<NetworkView>().RPC("Set", RPCMode.All, pp2);
+        }
+        if ((bool)player)
+        {
+            curVector = t.position - player.transform.position;
+            if (!(player.transform.position.x <= t.position.x))
+            {
+                e.transform.rotation = Quaternion.Euler(0f, 180f, 0f);
+            }
+            else
+            {
+                e.transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+            }
+        }
+        atking = true;
+        yield return new WaitForSeconds(0.5f);
+        atking = false;
+        if ((bool)player)
+        {
+            int num = UnityEngine.Random.Range(0, 3);
+            if (num == 0)
+            {
+                curVector = t.position - player.transform.position;
+            }
+            else
+            {
+                curVector = player.transform.position - t.position;
+            }
+            if (!(player.transform.position.x <= t.position.x))
+            {
+                e.transform.rotation = Quaternion.Euler(0f, 180f, 0f);
+            }
+            else
+            {
+                e.transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+            }
+            atking = true;
+            yield return new WaitForSeconds(2f);
+        }
+        atking = false;
+        yield return new WaitForSeconds(1.5f);
+        ATKING = false;
+    }
 
 	public GameObject djinPoint;
 
@@ -226,11 +153,6 @@ public class djin : EnemyScript
 				t.Translate(curVector.normalized * 6f * Time.deltaTime);
 			}
 		}
-	}
-
-	public virtual IEnumerator Attack(Vector3 pp)
-	{
-		return new _0024Attack_00242847(pp, this).GetEnumerator();
 	}
 
 	[RPC]
