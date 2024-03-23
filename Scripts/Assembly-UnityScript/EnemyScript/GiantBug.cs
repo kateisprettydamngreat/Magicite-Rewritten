be8c2539a739c815964e677d9d8c4f5eb5883cc0
Kate Skye <heyitskatehere@pm.me>
2024-03-23 19:57:55 -0400

    deboo giantbug

--- Scripts/Assembly-UnityScript/EnemyScript/GiantBug.cs
+++ Scripts/Assembly-UnityScript/EnemyScript/GiantBug.cs
@@ -2,81 +2,34 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
-using Boo.Lang;
 using UnityEngine;
 
 [Serializable]
 public class GiantBug : EnemyScript
 {
-	[Serializable]
-	[CompilerGenerated]
-	internal sealed class _0024Attack_00241858 : GenericGenerator<WaitForSeconds>
-	{
-		[Serializable]
-		[CompilerGenerated]
-		internal sealed class _0024 : GenericGeneratorEnumerator<WaitForSeconds>, IEnumerator
-		{
-			internal GiantBug _0024self__00241859;
+    public virtual IEnumerator Attack()
+    {
+        yield return new WaitForSeconds(1f);
 
-			public _0024(GiantBug self_)
-			{
-				_0024self__00241859 = self_;
-			}
+        if ((bool)player && Network.isServer)
+        {
+            GetComponent<NetworkView>().RPC("SpeedUp", RPCMode.All);
+            curVector = player.transform.position - t.position;
+            if (!(player.transform.position.x <= t.position.x))
+            {
+                e.transform.rotation = Quaternion.Euler(0f, 180f, 0f);
+            }
+            else
+            {
+                e.transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+            }
+            atking = true;
+            yield return new WaitForSeconds(2.5f);
+            GetComponent<NetworkView>().RPC("SpeedDown", RPCMode.All);
+        }
 
-			public override bool MoveNext()
-			{
-				int result;
-				switch (_state)
-				{
-				default:
-					result = (Yield(2, new WaitForSeconds(1f)) ? 1 : 0);
-					break;
-				case 2:
-					if ((bool)_0024self__00241859.player && Network.isServer)
-					{
-						_0024self__00241859.GetComponent<NetworkView>().RPC("SpeedUp", RPCMode.All);
-						_0024self__00241859.curVector = _0024self__00241859.player.transform.position - _0024self__00241859.t.position;
-						if (!(_0024self__00241859.player.transform.position.x <= _0024self__00241859.t.position.x))
-						{
-							_0024self__00241859.e.transform.rotation = Quaternion.Euler(0f, 180f, 0f);
-						}
-						else
-						{
-							_0024self__00241859.e.transform.rotation = Quaternion.Euler(0f, 0f, 0f);
-						}
-						_0024self__00241859.atking = true;
-						result = (Yield(3, new WaitForSeconds(2.5f)) ? 1 : 0);
-						break;
-					}
-					goto IL_0164;
-				case 3:
-					_0024self__00241859.GetComponent<NetworkView>().RPC("SpeedDown", RPCMode.All);
-					goto IL_0164;
-				case 1:
-					{
-						result = 0;
-						break;
-					}
-					IL_0164:
-					_0024self__00241859.atking = false;
-					goto default;
-				}
-				return (byte)result != 0;
-			}
-		}
-
-		internal GiantBug _0024self__00241860;
-
-		public _0024Attack_00241858(GiantBug self_)
-		{
-			_0024self__00241860 = self_;
-		}
-
-		public override IEnumerator<WaitForSeconds> GetEnumerator()
-		{
-			return new _0024(_0024self__00241860);
-		}
-	}
+        atking = false;
+    }
 
 	private GameObject player;
 
@@ -115,12 +68,6 @@ public class GiantBug : EnemyScript
 			t.Translate(curVector.normalized * 20f * Time.deltaTime);
 		}
 	}
-
-	public virtual IEnumerator Attack()
-	{
-		return new _0024Attack_00241858(this).GetEnumerator();
-	}
-
 	[RPC]
 	public virtual void SpeedUp()
 	{
