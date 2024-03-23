4a61738134301635cb7ad9accbb3bb9b5f01d214
Kate Skye <heyitskatehere@pm.me>
2024-03-23 19:46:18 -0400

    deboo fqueen

--- Scripts/Assembly-UnityScript/EnemyScript/fQueen.cs
+++ Scripts/Assembly-UnityScript/EnemyScript/fQueen.cs
@@ -2,126 +2,35 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
-using Boo.Lang;
 using UnityEngine;
 
 [Serializable]
 public class fQueen : EnemyScript
 {
-	[Serializable]
-	[CompilerGenerated]
-	internal sealed class _0024Summon_00242855 : GenericGenerator<WaitForSeconds>
-	{
-		[Serializable]
-		[CompilerGenerated]
-		internal sealed class _0024 : GenericGeneratorEnumerator<WaitForSeconds>, IEnumerator
-		{
-			internal fQueen _0024self__00242856;
-
-			public _0024(fQueen self_)
-			{
-				_0024self__00242856 = self_;
-			}
-
-			public override bool MoveNext()
-			{
-				int result;
-				switch (_state)
-				{
-				default:
-					_0024self__00242856.summoned = 0;
-					result = (Yield(2, new WaitForSeconds(20f)) ? 1 : 0);
-					break;
-				case 2:
-					if (Network.isServer)
-					{
-						goto IL_004a;
-					}
-					YieldDefault(1);
-					goto case 1;
-				case 3:
-					if (_0024self__00242856.summoned < 8)
-					{
-						_0024self__00242856.summoned++;
-						Network.Instantiate(Resources.Load("e/fairy"), _0024self__00242856.t.position, Quaternion.identity, 0);
-					}
-					goto IL_004a;
-				case 1:
-					{
-						result = 0;
-						break;
-					}
-					IL_004a:
-					result = (Yield(3, new WaitForSeconds(5f)) ? 1 : 0);
-					break;
-				}
-				return (byte)result != 0;
-			}
-		}
-
-		internal fQueen _0024self__00242857;
-
-		public _0024Summon_00242855(fQueen self_)
-		{
-			_0024self__00242857 = self_;
-		}
-
-		public override IEnumerator<WaitForSeconds> GetEnumerator()
-		{
-			return new _0024(_0024self__00242857);
-		}
-	}
-
-	[Serializable]
-	[CompilerGenerated]
-	internal sealed class _0024UpdateZrotation_00242858 : GenericGenerator<WaitForSeconds>
-	{
-		[Serializable]
-		[CompilerGenerated]
-		internal sealed class _0024 : GenericGeneratorEnumerator<WaitForSeconds>, IEnumerator
-		{
-			internal fQueen _0024self__00242859;
-
-			public _0024(fQueen self_)
-			{
-				_0024self__00242859 = self_;
-			}
-
-			public override bool MoveNext()
-			{
-				int result;
-				switch (_state)
-				{
-				default:
-					result = (Yield(2, new WaitForSeconds(2f)) ? 1 : 0);
-					break;
-				case 2:
-					_0024self__00242859.atking = true;
-					result = (Yield(3, new WaitForSeconds(1.5f)) ? 1 : 0);
-					break;
-				case 3:
-					_0024self__00242859.atking = false;
-					goto default;
-				case 1:
-					result = 0;
-					break;
-				}
-				return (byte)result != 0;
-			}
-		}
-
-		internal fQueen _0024self__00242860;
-
-		public _0024UpdateZrotation_00242858(fQueen self_)
-		{
-			_0024self__00242860 = self_;
-		}
-
-		public override IEnumerator<WaitForSeconds> GetEnumerator()
-		{
-			return new _0024(_0024self__00242860);
-		}
-	}
+    public virtual IEnumerator Summon()
+    {
+        summoned = 0;
+        yield return new WaitForSeconds(20f);
+        while (true)
+        {
+            if (Network.isServer)
+            {
+                if (summoned < 8)
+                {
+                    summoned++;
+                    Network.Instantiate(Resources.Load("e/fairy"), t.position, Quaternion.identity, 0);
+                }
+            }
+            yield return new WaitForSeconds(5f);
+        }
+    }
+    public virtual IEnumerator UpdateZrotation()
+    {
+        yield return new WaitForSeconds(2f);
+        atking = true;
+        yield return new WaitForSeconds(1.5f);
+        atking = false;
+    }
 
 	private GameObject player;
 
@@ -165,10 +74,6 @@ public class fQueen : EnemyScript
 		StartCoroutine_Auto(Summon());
 	}
 
-	public virtual IEnumerator Summon()
-	{
-		return new _0024Summon_00242855(this).GetEnumerator();
-	}
 
 	public virtual void SetPlayer(GameObject g)
 	{
@@ -198,10 +103,6 @@ public class fQueen : EnemyScript
 		}
 	}
 
-	public virtual IEnumerator UpdateZrotation()
-	{
-		return new _0024UpdateZrotation_00242858(this).GetEnumerator();
-	}
 
 	[RPC]
 	public virtual void Turn(int a)
