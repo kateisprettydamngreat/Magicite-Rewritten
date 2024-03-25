950c3e45c00393ab2f00344423762b8723f8e8d9
Kate Skye <heyitskatehere@pm.me>
2024-03-25 17:12:44 -0400

    deboo mino

--- Scripts/Assembly-UnityScript/EnemyScript/Mino.cs
+++ Scripts/Assembly-UnityScript/EnemyScript/Mino.cs
@@ -8,139 +8,40 @@ using UnityEngine;
 [Serializable]
 public class Mino : EnemyScript
 {
-	[Serializable]
-	[CompilerGenerated]
-	internal sealed class _0024ChargeRight_00242019 : GenericGenerator<WaitForSeconds>
-	{
-		[Serializable]
-		[CompilerGenerated]
-		internal sealed class _0024 : GenericGeneratorEnumerator<WaitForSeconds>, IEnumerator
-		{
-			internal Mino _0024self__00242020;
-
-			public _0024(Mino self_)
-			{
-				_0024self__00242020 = self_;
-			}
-
-			public override bool MoveNext()
-			{
-				int result;
-				switch (_state)
-				{
-				default:
-					if (!_0024self__00242020.charging && Network.isServer)
-					{
-						_0024self__00242020.charging = true;
-						_0024self__00242020.GetComponent<NetworkView>().RPC("Turn", RPCMode.All, 1);
-						_0024self__00242020.GetComponent<NetworkView>().RPC("ROAR", RPCMode.All);
-						result = (Yield(2, new WaitForSeconds(1f)) ? 1 : 0);
-						break;
-					}
-					goto IL_0114;
-				case 2:
-					_0024self__00242020.GetComponent<NetworkView>().RPC("ATK", RPCMode.All);
-					_0024self__00242020.spdd = 12;
-					_0024self__00242020.running = true;
-					result = (Yield(3, new WaitForSeconds(1.5f)) ? 1 : 0);
-					break;
-				case 3:
-					_0024self__00242020.GetComponent<NetworkView>().RPC("IDLE", RPCMode.All);
-					_0024self__00242020.charging = false;
-					_0024self__00242020.running = false;
-					goto IL_0114;
-				case 1:
-					{
-						result = 0;
-						break;
-					}
-					IL_0114:
-					YieldDefault(1);
-					goto case 1;
-				}
-				return (byte)result != 0;
-			}
-		}
-
-		internal Mino _0024self__00242021;
-
-		public _0024ChargeRight_00242019(Mino self_)
-		{
-			_0024self__00242021 = self_;
-		}
-
-		public override IEnumerator<WaitForSeconds> GetEnumerator()
-		{
-			return new _0024(_0024self__00242021);
-		}
-	}
-
-	[Serializable]
-	[CompilerGenerated]
-	internal sealed class _0024ChargeLeft_00242022 : GenericGenerator<WaitForSeconds>
-	{
-		[Serializable]
-		[CompilerGenerated]
-		internal sealed class _0024 : GenericGeneratorEnumerator<WaitForSeconds>, IEnumerator
-		{
-			internal Mino _0024self__00242023;
-
-			public _0024(Mino self_)
-			{
-				_0024self__00242023 = self_;
-			}
-
-			public override bool MoveNext()
-			{
-				int result;
-				switch (_state)
-				{
-				default:
-					if (!_0024self__00242023.charging && Network.isServer)
-					{
-						_0024self__00242023.charging = true;
-						_0024self__00242023.GetComponent<NetworkView>().RPC("Turn", RPCMode.All, 0);
-						_0024self__00242023.GetComponent<NetworkView>().RPC("ROAR", RPCMode.All);
-						result = (Yield(2, new WaitForSeconds(1f)) ? 1 : 0);
-						break;
-					}
-					goto IL_0114;
-				case 2:
-					_0024self__00242023.GetComponent<NetworkView>().RPC("ATK", RPCMode.All);
-					_0024self__00242023.spdd = -12;
-					_0024self__00242023.running = true;
-					result = (Yield(3, new WaitForSeconds(1.5f)) ? 1 : 0);
-					break;
-				case 3:
-					_0024self__00242023.GetComponent<NetworkView>().RPC("IDLE", RPCMode.All);
-					_0024self__00242023.charging = false;
-					_0024self__00242023.running = false;
-					goto IL_0114;
-				case 1:
-					{
-						result = 0;
-						break;
-					}
-					IL_0114:
-					YieldDefault(1);
-					goto case 1;
-				}
-				return (byte)result != 0;
-			}
-		}
-
-		internal Mino _0024self__00242024;
-
-		public _0024ChargeLeft_00242022(Mino self_)
-		{
-			_0024self__00242024 = self_;
-		}
-
-		public override IEnumerator<WaitForSeconds> GetEnumerator()
-		{
-			return new _0024(_0024self__00242024);
-		}
-	}
+    public virtual IEnumerator ChargeRight()
+    {
+        if (!charging && Network.isServer)
+        {
+            charging = true;
+            GetComponent<NetworkView>().RPC("Turn", RPCMode.All, 1);
+            GetComponent<NetworkView>().RPC("ROAR", RPCMode.All);
+            yield return new WaitForSeconds(1f);
+            GetComponent<NetworkView>().RPC("ATK", RPCMode.All);
+            spdd = 12;
+            running = true;
+            yield return new WaitForSeconds(1.5f);
+            GetComponent<NetworkView>().RPC("IDLE", RPCMode.All);
+            charging = false;
+            running = false;
+        }
+    }
+    public virtual IEnumerator ChargeLeft()
+    {
+        if (!charging && Network.isServer)
+        {
+            charging = true;
+            GetComponent<NetworkView>().RPC("Turn", RPCMode.All, 0);
+            GetComponent<NetworkView>().RPC("ROAR", RPCMode.All);
+            yield return new WaitForSeconds(1f);
+            GetComponent<NetworkView>().RPC("ATK", RPCMode.All);
+            spdd = -12;
+            running = true;
+            yield return new WaitForSeconds(1.5f);
+            GetComponent<NetworkView>().RPC("IDLE", RPCMode.All);
+            charging = false;
+            running = false;
+        }
+    }
 
 	private GameObject player;
 
@@ -222,16 +123,6 @@ public class Mino : EnemyScript
 			float num2 = (velocity.x = num);
 			Vector3 vector2 = (r.velocity = velocity);
 		}
-	}
-
-	public virtual IEnumerator ChargeRight()
-	{
-		return new _0024ChargeRight_00242019(this).GetEnumerator();
-	}
-
-	public virtual IEnumerator ChargeLeft()
-	{
-		return new _0024ChargeLeft_00242022(this).GetEnumerator();
 	}
 
 	[RPC]
