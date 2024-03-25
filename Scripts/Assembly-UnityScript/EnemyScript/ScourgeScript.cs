7fdbfbeed2161af39db01ca93f29d5f3ae5a03b0
Kate Skye <heyitskatehere@pm.me>
2024-03-25 17:57:53 -0400

    deboo scourgescript

--- Scripts/Assembly-UnityScript/EnemyScript/ScourgeScript.cs
+++ Scripts/Assembly-UnityScript/EnemyScript/ScourgeScript.cs
@@ -2,68 +2,26 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
-using Boo.Lang;
 using UnityEngine;
 
 [Serializable]
 public class ScourgeScript : EnemyScript
 {
-	[Serializable]
-	[CompilerGenerated]
-	internal sealed class _0024Attack_00242397 : GenericGenerator<WaitForSeconds>
-	{
-		[Serializable]
-		[CompilerGenerated]
-		internal sealed class _0024 : GenericGeneratorEnumerator<WaitForSeconds>, IEnumerator
-		{
-			internal ScourgeScript _0024self__00242398;
-
-			public _0024(ScourgeScript self_)
-			{
-				_0024self__00242398 = self_;
-			}
-
-			public override bool MoveNext()
-			{
-				int result;
-				switch (_state)
-				{
-				default:
-					_0024self__00242398.curVector = _0024self__00242398.player.transform.position - _0024self__00242398.t.position;
-					if (!(_0024self__00242398.player.transform.position.x <= _0024self__00242398.t.position.x))
-					{
-						_0024self__00242398.e.transform.rotation = Quaternion.Euler(0f, 180f, 0f);
-					}
-					else
-					{
-						_0024self__00242398.e.transform.rotation = Quaternion.Euler(0f, 0f, 0f);
-					}
-					_0024self__00242398.atking = true;
-					result = (Yield(2, new WaitForSeconds(2f)) ? 1 : 0);
-					break;
-				case 2:
-					_0024self__00242398.atking = false;
-					goto default;
-				case 1:
-					result = 0;
-					break;
-				}
-				return (byte)result != 0;
-			}
-		}
-
-		internal ScourgeScript _0024self__00242399;
-
-		public _0024Attack_00242397(ScourgeScript self_)
-		{
-			_0024self__00242399 = self_;
-		}
-
-		public override IEnumerator<WaitForSeconds> GetEnumerator()
-		{
-			return new _0024(_0024self__00242399);
-		}
-	}
+    public virtual IEnumerator Attack()
+    {
+        curVector = player.transform.position - t.position;
+        if (player.transform.position.x <= t.position.x)
+        {
+            e.transform.rotation = Quaternion.Euler(0f, 180f, 0f);
+        }
+        else
+        {
+            e.transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+        }
+        atking = true;
+        yield return new WaitForSeconds(2f);
+        atking = false;
+    }
 
 	private GameObject player;
 
@@ -112,11 +70,6 @@ public class ScourgeScript : EnemyScript
 		{
 			t.Translate(curVector.normalized * speed * Time.deltaTime);
 		}
-	}
-
-	public virtual IEnumerator Attack()
-	{
-		return new _0024Attack_00242397(this).GetEnumerator();
 	}
 
 	public override void K(bool l)
