6c7ce58824b3fa8e660f34710b0eee15179cc0de
Kate Skye <heyitskatehere@pm.me>
2024-03-25 17:55:06 -0400

    Deboo sarcher

--- Scripts/Assembly-UnityScript/EnemyScript/sArcher.cs
+++ Scripts/Assembly-UnityScript/EnemyScript/sArcher.cs
@@ -2,98 +2,45 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
-using Boo.Lang;
 using UnityEngine;
 
 [Serializable]
 public class sArcher : EnemyScript
 {
-	[Serializable]
-	[CompilerGenerated]
-	internal sealed class _0024Attack_00242941 : GenericGenerator<WaitForSeconds>
-	{
-		[Serializable]
-		[CompilerGenerated]
-		internal sealed class _0024 : GenericGeneratorEnumerator<WaitForSeconds>, IEnumerator
-		{
-			internal GameObject _0024g_00242942;
+    public IEnumerator Attack(Vector3 pp)
+    {
+        ATKING = true;
 
-			internal float _0024angle_00242943;
+        if (pp.x > transform.position.x)
+        {
+            GetComponent<NetworkView>().RPC("Turn", RPCMode.All, 1);
+        }
+        else
+        {
+            GetComponent<NetworkView>().RPC("Turn", RPCMode.All, 0);
+        }
 
-			internal UnityEngine.Object _0024ar_00242944;
+        yield return new WaitForSeconds(0.3f);
 
-			internal Vector3 _0024pp_00242945;
+        GetComponent<NetworkView>().RPC("A1", RPCMode.All);
 
-			internal sArcher _0024self__00242946;
+        yield return new WaitForSeconds(0.6f);
 
-			public _0024(Vector3 pp, sArcher self_)
-			{
-				_0024pp_00242945 = pp;
-				_0024self__00242946 = self_;
-			}
+        if (player)
+        {
+            target_pos = player.transform.position;
+            object_pos = transform.position;
+            target_pos.z = -20f;
+            target_pos.x = target_pos.x - object_pos.x;
+            target_pos.y = target_pos.y - object_pos.y;
+            float angle = Mathf.Atan2(target_pos.y, target_pos.x) * 57.29578f;
+            Network.Instantiate(Resources.Load("haz/arrow"), transform.position, Quaternion.Euler(new Vector3(0f, 0f, angle)), 0);
+        }
 
-			public override bool MoveNext()
-			{
-				int result;
-				switch (_state)
-				{
-				default:
-					_0024self__00242946.ATKING = true;
-					if (!(_0024pp_00242945.x <= _0024self__00242946.transform.position.x))
-					{
-						_0024self__00242946.GetComponent<NetworkView>().RPC("Turn", RPCMode.All, 1);
-					}
-					else
-					{
-						_0024self__00242946.GetComponent<NetworkView>().RPC("Turn", RPCMode.All, 0);
-					}
-					_0024g_00242942 = null;
-					result = (Yield(2, new WaitForSeconds(0.3f)) ? 1 : 0);
-					break;
-				case 2:
-					_0024self__00242946.GetComponent<NetworkView>().RPC("A1", RPCMode.All);
-					result = (Yield(3, new WaitForSeconds(0.6f)) ? 1 : 0);
-					break;
-				case 3:
-					if ((bool)_0024self__00242946.player)
-					{
-						_0024self__00242946.target_pos = _0024self__00242946.player.transform.position;
-						_0024self__00242946.object_pos = _0024self__00242946.transform.position;
-						_0024self__00242946.target_pos.z = -20f;
-						_0024self__00242946.target_pos.x = _0024self__00242946.target_pos.x - _0024self__00242946.object_pos.x;
-						_0024self__00242946.target_pos.y = _0024self__00242946.target_pos.y - _0024self__00242946.object_pos.y;
-						_0024angle_00242943 = Mathf.Atan2(_0024self__00242946.target_pos.y, _0024self__00242946.target_pos.x) * 57.29578f;
-						_0024ar_00242944 = Network.Instantiate(Resources.Load("haz/arrow"), _0024self__00242946.transform.position, Quaternion.Euler(new Vector3(0f, 0f, _0024angle_00242943)), 0);
-					}
-					result = (Yield(4, new WaitForSeconds(2.5f)) ? 1 : 0);
-					break;
-				case 4:
-					_0024self__00242946.ATKING = false;
-					YieldDefault(1);
-					goto case 1;
-				case 1:
-					result = 0;
-					break;
-				}
-				return (byte)result != 0;
-			}
-		}
+        yield return new WaitForSeconds(2.5f);
 
-		internal Vector3 _0024pp_00242947;
-
-		internal sArcher _0024self__00242948;
-
-		public _0024Attack_00242941(Vector3 pp, sArcher self_)
-		{
-			_0024pp_00242947 = pp;
-			_0024self__00242948 = self_;
-		}
-
-		public override IEnumerator<WaitForSeconds> GetEnumerator()
-		{
-			return new _0024(_0024pp_00242947, _0024self__00242948);
-		}
-	}
+        ATKING = false;
+    }
 
 	private Vector3 target_pos;
 
@@ -141,11 +88,6 @@ public class sArcher : EnemyScript
 		{
 			StartCoroutine_Auto(Attack(player.transform.position));
 		}
-	}
-
-	public virtual IEnumerator Attack(Vector3 pp)
-	{
-		return new _0024Attack_00242941(pp, this).GetEnumerator();
 	}
 
 	[RPC]
