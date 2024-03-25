319baaac8e9ba1dd399188277523b8f47250f04e
Kate Skye <heyitskatehere@pm.me>
2024-03-25 18:27:06 -0400

    Deboo tyrannox

--- Scripts/Assembly-UnityScript/EnemyScript/Tyrannox.cs
+++ Scripts/Assembly-UnityScript/EnemyScript/Tyrannox.cs
@@ -2,296 +2,81 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
-using Boo.Lang;
 using UnityEngine;
 
 [Serializable]
 public class Tyrannox : EnemyScript
 {
-	[Serializable]
-	[CompilerGenerated]
-	internal sealed class _0024Check_00242680 : GenericGenerator<WaitForSeconds>
-	{
-		[Serializable]
-		[CompilerGenerated]
-		internal sealed class _0024 : GenericGeneratorEnumerator<WaitForSeconds>, IEnumerator
-		{
-			internal int _0024prevHP_00242681;
+    public virtual IEnumerator Check()
+    {
+        int prevHP = default(int);
+        while (true)
+        {
+            prevHP = HP;
+            yield return new WaitForSeconds(3f);
+            if (HP < prevHP && Network.isServer)
+            {
+                int num = UnityEngine.Random.Range(0, 3);
+                if (num == 0 && !roaring)
+                {
+                    roaring = true;
+                    GetComponent<NetworkView>().RPC("ROAR", RPCMode.All);
+                    StartCoroutine(Meteor());
+                    yield return new WaitForSeconds(1f);
+                    roaring = false;
+                }
+            }
+            yield return new WaitForSeconds(1f);
+        }
+    }
 
-			internal int _0024num_00242682;
-
-			internal Tyrannox _0024self__00242683;
-
-			public _0024(Tyrannox self_)
-			{
-				_0024self__00242683 = self_;
-			}
-
-			public override bool MoveNext()
-			{
-				int result;
-				switch (_state)
-				{
-				default:
-					_0024prevHP_00242681 = default(int);
-					goto case 4;
-				case 4:
-					_0024prevHP_00242681 = _0024self__00242683.HP;
-					result = (Yield(2, new WaitForSeconds(3f)) ? 1 : 0);
-					break;
-				case 2:
-					if (_0024self__00242683.HP < _0024prevHP_00242681 && Network.isServer)
-					{
-						_0024num_00242682 = UnityEngine.Random.Range(0, 3);
-						if (_0024num_00242682 == 0 && !_0024self__00242683.roaring)
-						{
-							_0024self__00242683.roaring = true;
-							_0024self__00242683.GetComponent<NetworkView>().RPC("ROAR", RPCMode.All);
-							_0024self__00242683.StartCoroutine_Auto(_0024self__00242683.Meteor());
-							result = (Yield(3, new WaitForSeconds(1f)) ? 1 : 0);
-							break;
-						}
-					}
-					goto IL_00fa;
-				case 3:
-					_0024self__00242683.roaring = false;
-					goto IL_00fa;
-				case 1:
-					{
-						result = 0;
-						break;
-					}
-					IL_00fa:
-					result = (Yield(4, new WaitForSeconds(1f)) ? 1 : 0);
-					break;
-				}
-				return (byte)result != 0;
-			}
-		}
-
-		internal Tyrannox _0024self__00242684;
-
-		public _0024Check_00242680(Tyrannox self_)
-		{
-			_0024self__00242684 = self_;
-		}
-
-		public override IEnumerator<WaitForSeconds> GetEnumerator()
-		{
-			return new _0024(_0024self__00242684);
-		}
-	}
-
-	[Serializable]
-	[CompilerGenerated]
-	internal sealed class _0024Meteor_00242685 : GenericGenerator<WaitForSeconds>
-	{
-		[Serializable]
-		[CompilerGenerated]
-		internal sealed class _0024 : GenericGeneratorEnumerator<WaitForSeconds>, IEnumerator
-		{
-			internal int _0024i_00242686;
-
-			internal Tyrannox _0024self__00242687;
-
-			public _0024(Tyrannox self_)
-			{
-				_0024self__00242687 = self_;
-			}
-
-			public override bool MoveNext()
-			{
-				int result;
-				switch (_state)
-				{
-				default:
-					_0024i_00242686 = default(int);
-					if (Network.isServer)
-					{
-						_0024i_00242686 = 0;
-						goto IL_00b9;
-					}
-					goto IL_00c5;
-				case 2:
-					_0024i_00242686++;
-					goto IL_00b9;
-				case 1:
-					{
-						result = 0;
-						break;
-					}
-					IL_00c5:
-					YieldDefault(1);
-					goto case 1;
-					IL_00b9:
-					if (_0024i_00242686 < 6)
-					{
-						Network.Instantiate(Resources.Load("rckP"), new Vector3(_0024self__00242687.t.position.x + (float)UnityEngine.Random.Range(-20, 20), _0024self__00242687.t.position.y + 35f, 0f), Quaternion.identity, 0);
-						result = (Yield(2, new WaitForSeconds(0.3f)) ? 1 : 0);
-						break;
-					}
-					goto IL_00c5;
-				}
-				return (byte)result != 0;
-			}
-		}
-
-		internal Tyrannox _0024self__00242688;
-
-		public _0024Meteor_00242685(Tyrannox self_)
-		{
-			_0024self__00242688 = self_;
-		}
-
-		public override IEnumerator<WaitForSeconds> GetEnumerator()
-		{
-			return new _0024(_0024self__00242688);
-		}
-	}
-
-	[Serializable]
-	[CompilerGenerated]
-	internal sealed class _0024ChargeRight_00242689 : GenericGenerator<WaitForSeconds>
-	{
-		[Serializable]
-		[CompilerGenerated]
-		internal sealed class _0024 : GenericGeneratorEnumerator<WaitForSeconds>, IEnumerator
-		{
-			internal Tyrannox _0024self__00242690;
-
-			public _0024(Tyrannox self_)
-			{
-				_0024self__00242690 = self_;
-			}
-
-			public override bool MoveNext()
-			{
-				int result;
-				switch (_state)
-				{
-				default:
-					if (!_0024self__00242690.charging && Network.isServer)
-					{
-						_0024self__00242690.charging = true;
-						result = (Yield(2, new WaitForSeconds(1f)) ? 1 : 0);
-						break;
-					}
-					goto IL_0138;
-				case 2:
-					_0024self__00242690.GetComponent<NetworkView>().RPC("Turn", RPCMode.All, 1);
-					result = (Yield(3, new WaitForSeconds(0.2f)) ? 1 : 0);
-					break;
-				case 3:
-					_0024self__00242690.GetComponent<NetworkView>().RPC("ROAR", RPCMode.All);
-					result = (Yield(4, new WaitForSeconds(1.3f)) ? 1 : 0);
-					break;
-				case 4:
-					_0024self__00242690.GetComponent<NetworkView>().RPC("ATK", RPCMode.All);
-					_0024self__00242690.spdd = 6;
-					result = (Yield(5, new WaitForSeconds(3f)) ? 1 : 0);
-					break;
-				case 5:
-					_0024self__00242690.GetComponent<NetworkView>().RPC("IDLE", RPCMode.All);
-					_0024self__00242690.spdd = 0;
-					_0024self__00242690.charging = false;
-					goto IL_0138;
-				case 1:
-					{
-						result = 0;
-						break;
-					}
-					IL_0138:
-					YieldDefault(1);
-					goto case 1;
-				}
-				return (byte)result != 0;
-			}
-		}
-
-		internal Tyrannox _0024self__00242691;
-
-		public _0024ChargeRight_00242689(Tyrannox self_)
-		{
-			_0024self__00242691 = self_;
-		}
-
-		public override IEnumerator<WaitForSeconds> GetEnumerator()
-		{
-			return new _0024(_0024self__00242691);
-		}
-	}
-
-	[Serializable]
-	[CompilerGenerated]
-	internal sealed class _0024ChargeLeft_00242692 : GenericGenerator<WaitForSeconds>
-	{
-		[Serializable]
-		[CompilerGenerated]
-		internal sealed class _0024 : GenericGeneratorEnumerator<WaitForSeconds>, IEnumerator
-		{
-			internal Tyrannox _0024self__00242693;
-
-			public _0024(Tyrannox self_)
-			{
-				_0024self__00242693 = self_;
-			}
-
-			public override bool MoveNext()
-			{
-				int result;
-				switch (_state)
-				{
-				default:
-					if (!_0024self__00242693.charging && Network.isServer)
-					{
-						_0024self__00242693.charging = true;
-						result = (Yield(2, new WaitForSeconds(1f)) ? 1 : 0);
-						break;
-					}
-					goto IL_0139;
-				case 2:
-					_0024self__00242693.GetComponent<NetworkView>().RPC("Turn", RPCMode.All, 0);
-					result = (Yield(3, new WaitForSeconds(0.2f)) ? 1 : 0);
-					break;
-				case 3:
-					_0024self__00242693.GetComponent<NetworkView>().RPC("ROAR", RPCMode.All);
-					result = (Yield(4, new WaitForSeconds(1.3f)) ? 1 : 0);
-					break;
-				case 4:
-					_0024self__00242693.GetComponent<NetworkView>().RPC("ATK", RPCMode.All);
-					_0024self__00242693.spdd = -6;
-					result = (Yield(5, new WaitForSeconds(3f)) ? 1 : 0);
-					break;
-				case 5:
-					_0024self__00242693.GetComponent<NetworkView>().RPC("IDLE", RPCMode.All);
-					_0024self__00242693.spdd = 0;
-					_0024self__00242693.charging = false;
-					goto IL_0139;
-				case 1:
-					{
-						result = 0;
-						break;
-					}
-					IL_0139:
-					YieldDefault(1);
-					goto case 1;
-				}
-				return (byte)result != 0;
-			}
-		}
-
-		internal Tyrannox _0024self__00242694;
-
-		public _0024ChargeLeft_00242692(Tyrannox self_)
-		{
-			_0024self__00242694 = self_;
-		}
-
-		public override IEnumerator<WaitForSeconds> GetEnumerator()
-		{
-			return new _0024(_0024self__00242694);
-		}
-	}
+    public virtual IEnumerator Meteor()
+    {
+        if (Network.isServer)
+        {
+            for (int i = 0; i < 6; i++)
+            {
+                Network.Instantiate(Resources.Load("rckP"), new Vector3(t.position.x + (float)UnityEngine.Random.Range(-20, 20), t.position.y + 35f, 0f), Quaternion.identity, 0);
+                yield return new WaitForSeconds(0.3f);
+            }
+        }
+    }
+    public virtual IEnumerator ChargeRight()
+    {
+        if (!charging && Network.isServer)
+        {
+            charging = true;
+            yield return new WaitForSeconds(1f);
+            GetComponent<NetworkView>().RPC("Turn", RPCMode.All, 1);
+            yield return new WaitForSeconds(0.2f);
+            GetComponent<NetworkView>().RPC("ROAR", RPCMode.All);
+            yield return new WaitForSeconds(1.3f);
+            GetComponent<NetworkView>().RPC("ATK", RPCMode.All);
+            spdd = 6;
+            yield return new WaitForSeconds(3f);
+            GetComponent<NetworkView>().RPC("IDLE", RPCMode.All);
+            spdd = 0;
+            charging = false;
+        }
+    }
+    public virtual IEnumerator ChargeLeft()
+    {
+        if (!charging && Network.isServer)
+        {
+            charging = true;
+            yield return new WaitForSeconds(1f);
+            GetComponent<NetworkView>().RPC("Turn", RPCMode.All, 0);
+            yield return new WaitForSeconds(0.2f);
+            GetComponent<NetworkView>().RPC("ROAR", RPCMode.All);
+            yield return new WaitForSeconds(1.3f);
+            GetComponent<NetworkView>().RPC("ATK", RPCMode.All);
+            spdd = -6;
+            yield return new WaitForSeconds(3f);
+            GetComponent<NetworkView>().RPC("IDLE", RPCMode.All);
+            spdd = 0;
+            charging = false;
+        }
+    }
 
 	private GameObject player;
 
@@ -390,26 +175,6 @@ public class Tyrannox : EnemyScript
 			float num6 = (velocity3.x = num5);
 			Vector3 vector6 = (r.velocity = velocity3);
 		}
-	}
-
-	public virtual IEnumerator Check()
-	{
-		return new _0024Check_00242680(this).GetEnumerator();
-	}
-
-	public virtual IEnumerator Meteor()
-	{
-		return new _0024Meteor_00242685(this).GetEnumerator();
-	}
-
-	public virtual IEnumerator ChargeRight()
-	{
-		return new _0024ChargeRight_00242689(this).GetEnumerator();
-	}
-
-	public virtual IEnumerator ChargeLeft()
-	{
-		return new _0024ChargeLeft_00242692(this).GetEnumerator();
 	}
 
 	[RPC]
