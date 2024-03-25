86908102931560ea06ebffc45cad09108d71041e
Kate Skye <heyitskatehere@pm.me>
2024-03-25 18:43:30 -0400

    Deboo SpiderScript

--- Scripts/Assembly-UnityScript/EnemyScript/SpiderScript.cs
+++ Scripts/Assembly-UnityScript/EnemyScript/SpiderScript.cs
@@ -2,149 +2,46 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
-using Boo.Lang;
 using UnityEngine;
 
 [Serializable]
 public class SpiderScript : EnemyScript
 {
-	[Serializable]
-	[CompilerGenerated]
-	internal sealed class _0024ChargeRight_00242603 : GenericGenerator<WaitForSeconds>
-	{
-		[Serializable]
-		[CompilerGenerated]
-		internal sealed class _0024 : GenericGeneratorEnumerator<WaitForSeconds>, IEnumerator
-		{
-			internal int _0024_0024947_00242604;
+    public virtual IEnumerator ChargeRight()
+    {
+        if (!charging && Network.isServer)
+        {
+            int num = 15;
+            Vector3 velocity = r.velocity;
+            velocity.y = num;
+            r.velocity = velocity;
+            charging = true;
+            GetComponent<NetworkView>().RPC("Turn", RPCMode.All, 1);
+            GetComponent<NetworkView>().RPC("ATK", RPCMode.All);
+            spdd = 5;
+            yield return new WaitForSeconds(1f);
+            GetComponent<NetworkView>().RPC("IDLE", RPCMode.All);
+            charging = false;
+        }
+    }
 
-			internal Vector3 _0024_0024948_00242605;
-
-			internal SpiderScript _0024self__00242606;
-
-			public _0024(SpiderScript self_)
-			{
-				_0024self__00242606 = self_;
-			}
-
-			public override bool MoveNext()
-			{
-				int result;
-				switch (_state)
-				{
-				default:
-					if (!_0024self__00242606.charging && Network.isServer)
-					{
-						int num = (_0024_0024947_00242604 = 15);
-						Vector3 vector = (_0024_0024948_00242605 = _0024self__00242606.r.velocity);
-						float num2 = (_0024_0024948_00242605.y = _0024_0024947_00242604);
-						Vector3 vector3 = (_0024self__00242606.r.velocity = _0024_0024948_00242605);
-						_0024self__00242606.charging = true;
-						_0024self__00242606.GetComponent<NetworkView>().RPC("Turn", RPCMode.All, 1);
-						_0024self__00242606.GetComponent<NetworkView>().RPC("ATK", RPCMode.All);
-						_0024self__00242606.spdd = 5;
-						result = (Yield(2, new WaitForSeconds(1f)) ? 1 : 0);
-						break;
-					}
-					goto IL_011b;
-				case 2:
-					_0024self__00242606.GetComponent<NetworkView>().RPC("IDLE", RPCMode.All);
-					_0024self__00242606.charging = false;
-					goto IL_011b;
-				case 1:
-					{
-						result = 0;
-						break;
-					}
-					IL_011b:
-					YieldDefault(1);
-					goto case 1;
-				}
-				return (byte)result != 0;
-			}
-		}
-
-		internal SpiderScript _0024self__00242607;
-
-		public _0024ChargeRight_00242603(SpiderScript self_)
-		{
-			_0024self__00242607 = self_;
-		}
-
-		public override IEnumerator<WaitForSeconds> GetEnumerator()
-		{
-			return new _0024(_0024self__00242607);
-		}
-	}
-
-	[Serializable]
-	[CompilerGenerated]
-	internal sealed class _0024ChargeLeft_00242608 : GenericGenerator<WaitForSeconds>
-	{
-		[Serializable]
-		[CompilerGenerated]
-		internal sealed class _0024 : GenericGeneratorEnumerator<WaitForSeconds>, IEnumerator
-		{
-			internal int _0024_0024949_00242609;
-
-			internal Vector3 _0024_0024950_00242610;
-
-			internal SpiderScript _0024self__00242611;
-
-			public _0024(SpiderScript self_)
-			{
-				_0024self__00242611 = self_;
-			}
-
-			public override bool MoveNext()
-			{
-				int result;
-				switch (_state)
-				{
-				default:
-					if (!_0024self__00242611.charging && Network.isServer)
-					{
-						int num = (_0024_0024949_00242609 = 15);
-						Vector3 vector = (_0024_0024950_00242610 = _0024self__00242611.r.velocity);
-						float num2 = (_0024_0024950_00242610.y = _0024_0024949_00242609);
-						Vector3 vector3 = (_0024self__00242611.r.velocity = _0024_0024950_00242610);
-						_0024self__00242611.charging = true;
-						_0024self__00242611.GetComponent<NetworkView>().RPC("Turn", RPCMode.All, 0);
-						_0024self__00242611.GetComponent<NetworkView>().RPC("ATK", RPCMode.All);
-						_0024self__00242611.spdd = -5;
-						result = (Yield(2, new WaitForSeconds(1f)) ? 1 : 0);
-						break;
-					}
-					goto IL_011c;
-				case 2:
-					_0024self__00242611.GetComponent<NetworkView>().RPC("IDLE", RPCMode.All);
-					_0024self__00242611.charging = false;
-					goto IL_011c;
-				case 1:
-					{
-						result = 0;
-						break;
-					}
-					IL_011c:
-					YieldDefault(1);
-					goto case 1;
-				}
-				return (byte)result != 0;
-			}
-		}
-
-		internal SpiderScript _0024self__00242612;
-
-		public _0024ChargeLeft_00242608(SpiderScript self_)
-		{
-			_0024self__00242612 = self_;
-		}
-
-		public override IEnumerator<WaitForSeconds> GetEnumerator()
-		{
-			return new _0024(_0024self__00242612);
-		}
-	}
+    public virtual IEnumerator ChargeLeft()
+    {
+        if (!charging && Network.isServer)
+        {
+            int num = 15;
+            Vector3 velocity = r.velocity;
+            velocity.y = num;
+            r.velocity = velocity;
+            charging = true;
+            GetComponent<NetworkView>().RPC("Turn", RPCMode.All, 0);
+            GetComponent<NetworkView>().RPC("ATK", RPCMode.All);
+            spdd = -5;
+            yield return new WaitForSeconds(1f);
+            GetComponent<NetworkView>().RPC("IDLE", RPCMode.All);
+            charging = false;
+        }
+    }
 
 	private GameObject player;
 
@@ -201,16 +98,6 @@ public class SpiderScript : EnemyScript
 			float num2 = (velocity.x = num);
 			Vector3 vector2 = (r.velocity = velocity);
 		}
-	}
-
-	public virtual IEnumerator ChargeRight()
-	{
-		return new _0024ChargeRight_00242603(this).GetEnumerator();
-	}
-
-	public virtual IEnumerator ChargeLeft()
-	{
-		return new _0024ChargeLeft_00242608(this).GetEnumerator();
 	}
 
 	[RPC]
