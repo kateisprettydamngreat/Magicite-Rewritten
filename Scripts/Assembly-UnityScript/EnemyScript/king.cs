fcb51dd56b8b6d6fe4f360558d00cf57bbdd4ac3
Kate Skye <heyitskatehere@pm.me>
2024-03-25 16:43:15 -0400

    deboo king

--- Scripts/Assembly-UnityScript/EnemyScript/king.cs
+++ Scripts/Assembly-UnityScript/EnemyScript/king.cs
@@ -2,164 +2,68 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
-using Boo.Lang;
 using UnityEngine;
 
 [Serializable]
 public class king : EnemyScript
 {
-	[Serializable]
-	[CompilerGenerated]
-	internal sealed class _0024Attack_00242898 : GenericGenerator<WaitForSeconds>
-	{
-		[Serializable]
-		[CompilerGenerated]
-		internal sealed class _0024 : GenericGeneratorEnumerator<WaitForSeconds>, IEnumerator
-		{
-			internal Vector3 _0024pp2_00242899;
+    public virtual IEnumerator Attack(Vector3 pp)
+    {
+        ATKING = true;
+        atking = false;
+        MOVING = false;
+        yield return new WaitForSeconds(1f);
 
-			internal GameObject _0024g_00242900;
+        GetComponent<NetworkView>().RPC("ATK", RPCMode.All);
+        yield return new WaitForSeconds(0.5f);
 
-			internal king _0024self__00242901;
+        GetComponent<NetworkView>().RPC("ATK2", RPCMode.All);
+        if (player)
+        {
+            Vector3 pp2 = player.transform.position;
+            if (Network.isServer)
+            {
+                GameObject g = null;
+                g = (GameObject)Network.Instantiate(Resources.Load("haz/whelpFire"), transform.position, Quaternion.identity, 0);
+                g.GetComponent<NetworkView>().RPC("Set", RPCMode.All, pp2);
+            }
+        }
+        yield return new WaitForSeconds(1f);
 
-			public _0024(king self_)
-			{
-				_0024self__00242901 = self_;
-			}
+        if (player && Network.isServer)
+        {
+            curVector = player.transform.position - t.position;
+            if (player.transform.position.x > transform.position.x)
+            {
+                GetComponent<NetworkView>().RPC("Turn", RPCMode.All, 1);
+            }
+            else
+            {
+                GetComponent<NetworkView>().RPC("Turn", RPCMode.All, 0);
+            }
+        }
+        MOVING = true;
+        yield return new WaitForSeconds(2f);
 
-			public override bool MoveNext()
-			{
-				int result;
-				switch (_state)
-				{
-				default:
-					_0024self__00242901.ATKING = true;
-					_0024self__00242901.atking = false;
-					_0024self__00242901.MOVING = false;
-					result = (Yield(2, new WaitForSeconds(1f)) ? 1 : 0);
-					break;
-				case 2:
-					_0024self__00242901.GetComponent<NetworkView>().RPC("ATK", RPCMode.All);
-					result = (Yield(3, new WaitForSeconds(0.5f)) ? 1 : 0);
-					break;
-				case 3:
-					_0024self__00242901.GetComponent<NetworkView>().RPC("ATK2", RPCMode.All);
-					if ((bool)_0024self__00242901.player)
-					{
-						_0024pp2_00242899 = _0024self__00242901.player.transform.position;
-						if (Network.isServer)
-						{
-							_0024g_00242900 = null;
-							_0024g_00242900 = (GameObject)Network.Instantiate(Resources.Load("haz/whelpFire"), _0024self__00242901.transform.position, Quaternion.identity, 0);
-							_0024g_00242900.GetComponent<NetworkView>().RPC("Set", RPCMode.All, _0024pp2_00242899);
-						}
-						_0024g_00242900.GetComponent<NetworkView>().RPC("Set", RPCMode.All, _0024pp2_00242899);
-					}
-					result = (Yield(4, new WaitForSeconds(1f)) ? 1 : 0);
-					break;
-				case 4:
-					if ((bool)_0024self__00242901.player && Network.isServer)
-					{
-						_0024self__00242901.curVector = _0024self__00242901.player.transform.position - _0024self__00242901.t.position;
-						if (!(_0024self__00242901.player.transform.position.x <= _0024self__00242901.transform.position.x))
-						{
-							_0024self__00242901.GetComponent<NetworkView>().RPC("Turn", RPCMode.All, 1);
-						}
-						else
-						{
-							_0024self__00242901.GetComponent<NetworkView>().RPC("Turn", RPCMode.All, 0);
-						}
-					}
-					_0024self__00242901.MOVING = true;
-					result = (Yield(5, new WaitForSeconds(2f)) ? 1 : 0);
-					break;
-				case 5:
-					_0024self__00242901.ATKING = false;
-					YieldDefault(1);
-					goto case 1;
-				case 1:
-					result = 0;
-					break;
-				}
-				return (byte)result != 0;
-			}
-		}
+        ATKING = false;
+    }
 
-		internal king _0024self__00242902;
-
-		public _0024Attack_00242898(king self_)
-		{
-			_0024self__00242902 = self_;
-		}
-
-		public override IEnumerator<WaitForSeconds> GetEnumerator()
-		{
-			return new _0024(_0024self__00242902);
-		}
-	}
-
-	[Serializable]
-	[CompilerGenerated]
-	internal sealed class _0024ATK2_00242903 : GenericGenerator<WaitForSeconds>
-	{
-		[Serializable]
-		[CompilerGenerated]
-		internal sealed class _0024 : GenericGeneratorEnumerator<WaitForSeconds>, IEnumerator
-		{
-			internal int _0024i_00242904;
-
-			internal king _0024self__00242905;
-
-			public _0024(king self_)
-			{
-				_0024self__00242905 = self_;
-			}
-
-			public override bool MoveNext()
-			{
-				int result;
-				switch (_state)
-				{
-				default:
-					_0024i_00242904 = default(int);
-					_0024self__00242905.CRAZE = true;
-					result = (Yield(2, new WaitForSeconds(0.3f)) ? 1 : 0);
-					break;
-				case 2:
-					for (_0024i_00242904 = 0; _0024i_00242904 < 4; _0024i_00242904++)
-					{
-						_0024self__00242905.swords[_0024i_00242904].GetComponent<Collider>().enabled = true;
-					}
-					result = (Yield(3, new WaitForSeconds(0.4f)) ? 1 : 0);
-					break;
-				case 3:
-					for (_0024i_00242904 = 0; _0024i_00242904 < 4; _0024i_00242904++)
-					{
-						_0024self__00242905.swords[_0024i_00242904].GetComponent<Collider>().enabled = false;
-					}
-					_0024self__00242905.CRAZE = false;
-					YieldDefault(1);
-					goto case 1;
-				case 1:
-					result = 0;
-					break;
-				}
-				return (byte)result != 0;
-			}
-		}
-
-		internal king _0024self__00242906;
-
-		public _0024ATK2_00242903(king self_)
-		{
-			_0024self__00242906 = self_;
-		}
-
-		public override IEnumerator<WaitForSeconds> GetEnumerator()
-		{
-			return new _0024(_0024self__00242906);
-		}
-	}
+    public virtual IEnumerator ATK2()
+    {
+        int i;
+        CRAZE = true;
+        yield return new WaitForSeconds(0.3f);
+        for (i = 0; i < 4; i++)
+        {
+            swords[i].GetComponent<Collider>().enabled = true;
+        }
+        yield return new WaitForSeconds(0.4f);
+        for (i = 0; i < 4; i++)
+        {
+            swords[i].GetComponent<Collider>().enabled = false;
+        }
+        CRAZE = false;
+    }
 
 	public AudioClip roar;
 
@@ -259,10 +163,6 @@ public class king : EnemyScript
 		}
 	}
 
-	public virtual IEnumerator Attack(Vector3 pp)
-	{
-		return new _0024Attack_00242898(this).GetEnumerator();
-	}
 
 	[RPC]
 	public virtual void ATK()
@@ -290,9 +190,4 @@ public class king : EnemyScript
 		}
 	}
 
-	[RPC]
-	public virtual IEnumerator ATK2()
-	{
-		return new _0024ATK2_00242903(this).GetEnumerator();
-	}
 }
